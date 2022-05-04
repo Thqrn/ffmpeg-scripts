f6a7a9fe6a07910581cdbcef7972d5d086406a06
Frosty7473 <frostlord7473@gmail.com>
2022-05-03 20:40:55 -0400

    small encoding fix

--- !replace audio.bat
+++ !replace audio.bat
@@ -1,4 +1,4 @@
 @echo off
 set /p lowqualmusic=Please drag the desired file here, it must be an audio file: 
 set /p musicstarttime=Enter a specific start time of the music in seconds: 
-for %%a in (%*) do ffmpeg -loglevel warning -stats -i %%a -ss %musicstarttime% -i %lowqualmusic% -c:v copy -preset slow -c:a copy -map 0:v:0 -map 1:a:0 -shortest "%%~dpna (added audio).mp4"
+for %%a in (%*) do ffmpeg -loglevel warning -stats -i %%a -ss %musicstarttime% -i %lowqualmusic% -c:v copy -preset slow -c:a aac -map 0:v:0 -map 1:a:0 -shortest "%%~dpna (added audio).mp4"
