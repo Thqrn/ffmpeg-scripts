65e0e412b5bf1272f8a286f09ce1d47ae7d86ae2
Frosty7473 <frostlord7473@gmail.com>
2023-08-07 14:36:23 -0400

    updating copyright

--- !replace audio.bat
+++ !replace audio.bat
@@ -2,6 +2,8 @@
 :: This program is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
 :: You should have received a copy of the GNU General Public License along with this program. If not, see http://www.gnu.org/licenses/.
 
+:: Copyright 2023 Thqrn. Licensed under the GNU General Public License v3.0.
+
 :: @froest on Discord
 :: https://github.com/Thqrn/ffmpeg-scripts
 
@@ -9,7 +11,7 @@
 
 @echo off
 set /p lowqualmusic=Please drag the desired file here, it must be an audio file: 
-set /p name=Enter the music title: 
+call :getname %lowqualmusic%
 set /p musicstarttime=Enter a specific start time of the music in seconds: 
 choice /c CR /n /m "Do you want to [C]opy the streams or [R]eencode?"
 if %errorlevel% == 1 (for %%a in (%*) do ffmpeg -loglevel warning -stats -i %%a -ss %musicstarttime% -i %lowqualmusic% -c copy -map 0:v:0 -map 1:a:0 -shortest "%%~dpna (plus %name%).mp4")
@@ -17,3 +19,7 @@ if %errorlevel% == 2 (for %%a in (%*) do ffmpeg -loglevel warning -stats -i %%a 
 where /q ffplay || exit
 if not exist "C:\Windows\Media\notify.wav" (exit) else ffplay "C:\Windows\Media\notify.wav" -volume 50 -autoexit -showmode 0 -loglevel quiet
 exit
+
+:getname
+set name=%~n1
+goto:eof
